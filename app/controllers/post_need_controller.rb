29da799c74f9550948608bd56cd787a2117e6fb4
Minnie <minniew@andrew.cmu.edu>
2017-06-08 15:35:37 -0400

    added controllers for all models

---
+++ app/controllers/post_need_controller.rb
@@ -0,0 +1,2 @@
+class PostNeedController < ApplicationController
+end
---
+++ test/controllers/need_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class NeedControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
