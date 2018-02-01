24168291ca40d002ae1b1807e2dc7d9a16860c3a
Sean Park <spark3@andrew.cmu.edu>
2018-02-01 12:25:22 -0500

    minor style and formatting changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,7 @@ module ApplicationHelper
   def google_map(center)
   "https://maps.googleapis.com/maps/api/staticmap?center=#{center}&size=300x300&zoom=17"
   end
+  def format_time(time)
+  	time.in_time_zone(Time.zone).strftime('%B %e at %l:%M %p')
+  end
 end
