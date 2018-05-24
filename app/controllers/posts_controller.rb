3c9234008d07c0c7330bf39faba6354cc0894395
Xinran Yuan <xinrany@andrew.cmu.edu>
2018-05-23 22:55:20 -0400

    issues fixed except email

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -59,7 +59,7 @@ def create
 				pn.save!
 			end
 		end
-		#UserNotifier.send_post_notification(@post).deliver_later
+		UserNotifier.send_post_notification(@post).deliver_later
 		redirect_to posts_path, notice: "Added post!"
 	else
 		@all_needs = Need.by_category
