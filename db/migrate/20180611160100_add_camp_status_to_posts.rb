4906a5e96ec4e5e54443bf194602052141a58849
Xinran Yuan <xinrany@andrew.cmu.edu>
2018-06-12 09:32:42 -0400

    migration

--- db/migrate/20180611160100_add_camp_status_to_posts.rb
+++ db/migrate/20180611160100_add_camp_status_to_posts.rb
@@ -1,5 +1,5 @@
 class AddCampStatusToPosts < ActiveRecord::Migration
   def change
-    add_column :posts, :camp_status, :string, default: "requests"
+    add_column :posts, :camp_status, :string
   end
 end
